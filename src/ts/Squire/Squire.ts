e36d5e0438b629a25486d3e2725fcebd34c3f86c
Cristina <cristinajmartin98@gmail.com>
2023-01-23 21:08:17 +0100

    Add Squire class module

---
+++ src/ts/Squire/Squire.ts
@@ -0,0 +1,44 @@
+import {
+  Character,
+  type CharacterStructure,
+  type CharacterDataStructure,
+} from "../Character/Character";
+import Fighter from "../Fighter/Fighter";
+
+interface SquireStructure extends CharacterStructure {
+  serves: Fighter;
+  kissAssLevel: number;
+}
+
+class Squire extends Character implements SquireStructure {
+  constructor(
+    characterData: CharacterDataStructure,
+    public kissAssLevel: number,
+    public serves: Fighter
+  ) {
+    super(characterData);
+    this.kissAssLevel = this.kissAssLevelFilter(kissAssLevel);
+
+    if (serves instanceof Fighter) {
+      this.serves = serves;
+    }
+  }
+
+  communicate(): string {
+    return `${super.communicate()}I'm a loser`;
+  }
+
+  private kissAssLevelFilter(kissAssLevel: number): number {
+    if (kissAssLevel < 0) {
+      return 0;
+    }
+
+    if (kissAssLevel > 10) {
+      return 10;
+    }
+
+    return kissAssLevel;
+  }
+}
+
+export default Squire;
